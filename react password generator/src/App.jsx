988f5645fd25347da42e5519e231177c22337b1a
unknown <oraza7969@gmail.com>
2024-01-05 10:59:32 -0800

    password generator

--- react password generator/src/App.jsx
+++ react password generator/src/App.jsx
@@ -1,25 +1,18 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
 
-import './App.css'
-
-function App() {
- 
+const App = () => {
+  const [password, setPassword] = useState('abcd');
+  const [length, setLength] = useState('');
+  const [number, setNumber] = useState(false);
+  const [symbol, setSymbol] = useState(false);
 
   return (
     <>
-     <section className='align-center items-center'>
-      <div className="container rounded p-6 bg-gray-500 items-center w-[20%]">
-<form id='pg-form'>
-  <div className='flex'>
-    <input className='w-[100%] p-1 rounded' type="text" placeholder='Min 8 Char' readOnly />
-    <div className='p-1 px-3 text-white rounded bg-blue-500'><i class="fa-regular fa-clipboard"></i></div>
-  </div>
-</form>
-
-      </div>
-     </section>
+      <h2>{password}</h2>
+      <label htmlFor="length">{length}</label>
+      <input type="range" id='' min={8} max={20} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
